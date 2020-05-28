830bb9246b4d590b1d5f3fc30651935df63809be
Adrien Ruellou <aruellou@Adriens-MacBook-Pro.local>
2020-05-28 19:19:58 +0200

    Views ready to work

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -7,23 +7,27 @@
     dark
   >
     <v-list-item class="px-2">
-      <v-img
-        alt="Velero Logo"
-        class="shrink mr-2"
-        contain
-        src="@/assets/velero-icon.svg"
-        transition="scale-transition"
-        width="100"
-      />
+      <v-list-item-icon>
+        <v-img
+          alt="Velero Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/velero-icon.svg"
+          transition="scale-transition"
+          width="50"
+        />
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-img
+          alt="Velero Text"
+          class="shrink mr-2"
+          contain
+          src="@/assets/velero-text.svg"
+          transition="scale-transition"
+          width="200"
+        />
+      </v-list-item-content>
 
-      <v-img
-        alt="Velero Logo"
-        class="shrink mr-2"
-        contain
-        src="@/assets/velero-text.svg"
-        transition="scale-transition"
-        width="200"
-      />
       <v-btn icon @click.stop="mini = !mini">
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
@@ -51,8 +55,9 @@ export default {
     return {
       drawer: true,
       links: [
-        { title: "Backups", icon: "mdi-backup-restore", route: "/" },
-        { title: "Schedules", icon: "mdi-clock-outline", route: "/schedule" },
+        { title: "Dashboard", icon: "mdi-home", route: "/" },
+        { title: "Backups", icon: "mdi-backup-restore", route: "/backups" },
+        { title: "Schedules", icon: "mdi-clock-outline", route: "/schedules" },
         { title: "Settings", icon: "mdi-tune", route: "/settings" }
       ],
       mini: false
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Dashboard from "@/views/Dashboard.vue";
+import Backups from "@/views/Backups.vue";
+import Schedules from "@/views/Schedules.vue";
+import Settings from "@/views/Settings.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Dashboard",
+    component: Dashboard
   },
   {
-    path: "/about",
-    name: "About",
-    component: () => import("../views/About.vue")
+    path: "/backups",
+    name: "Backups",
+    component: Backups
+  },
+  {
+    path: "/schedules",
+    name: "Schedules",
+    component: Schedules
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings
   }
 ];
 
