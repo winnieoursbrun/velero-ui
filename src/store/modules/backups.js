330552c2e2bb021353a50df18ee9ce6f1956a5dc
Adrien Ruellou <aruellou@Adriens-MacBook-Pro.local>
2020-07-03 01:54:56 +0200

    Working on backups

--- src/store/modules/backups.js
+++ src/store/modules/backups.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { flattenObject } from "@/tools";
 
 const state = {
   backups: undefined,
+  keys: undefined,
   error: undefined,
   loading: false
 };
@@ -10,31 +12,32 @@ const getters = {
   isError: state => !!state.error,
   isLoading: state => state.loading,
   backups: state => state.backups,
+  keys: state => state.keys,
   error: state => state.error
 };
 
 const actions = {
-  get({ commit }) {
+  getBackups({ commit }) {
     commit("BACKUPS_LOADING");
     axios
       .get("/api/apis/velero.io/v1/namespaces/velero/backups")
       .then(result => {
         if (result.status == 200) {
-          let newList = [];
+          // let newList = [];
           try {
-            result.data.items.forEach(item => {
-              let tmp = {};
-              tmp["id"] = item.metadata.uid;
-              tmp["name"] = item.metadata.name;
-              tmp["creationTimestamp"] = item.metadata.creationTimestamp;
-              tmp["namespace"] = item.metadata.namespace;
-              tmp["ttl"] = item.spec.ttl;
-              tmp["volumeSnapshotLocations"] =
-                item.spec.volumeSnapshotLocations[0];
-              tmp["errors"] = item.status.errors;
-              newList.push(tmp);
-            });
-            commit("BACKUPS_UPDATE", newList);
+            // result.data.items.forEach(item => {
+            //   let tmp = {};
+            //   // tmp["id"] = item.metadata.uid;
+            //   // tmp["name"] = item.metadata.name;
+            //   // tmp["creationTimestamp"] = item.metadata.creationTimestamp;
+            //   // tmp["namespace"] = item.metadata.namespace;
+            //   // tmp["ttl"] = item.spec.ttl;
+            //   // tmp["volumeSnapshotLocations"] =
+            //   //   item.spec.volumeSnapshotLocations[0];
+            //   // tmp["errors"] = item.status.errors;
+            //   newList.push(tmp);
+            // });
+            commit("BACKUPS_UPDATE", result.data.items);
           } catch (error) {
             commit("BACKUPS_ERROR", error);
           }
@@ -45,12 +48,60 @@ const actions = {
       .catch(error => {
         commit("BACKUPS_ERROR", error);
       });
+  },
+  getBackup({ commit }, id) {
+    commit("BACKUPS_LOADING");
+    axios
+      .get(`/api/apis/velero.io/v1/namespaces/velero/backups/${id}`)
+      .then(result => {
+        if (result.status == 200) {
+          // let newList = [];
+          console.log(result.data);
+          // try {
+          //   result.data.items.forEach(item => {
+          //     let tmp = {};
+          //     tmp["id"] = item.metadata.uid;
+          //     tmp["name"] = item.metadata.name;
+          //     tmp["creationTimestamp"] = item.metadata.creationTimestamp;
+          //     tmp["namespace"] = item.metadata.namespace;
+          //     tmp["ttl"] = item.spec.ttl;
+          //     tmp["volumeSnapshotLocations"] =
+          //       item.spec.volumeSnapshotLocations[0];
+          //     tmp["errors"] = item.status.errors;
+          //     newList.push(tmp);
+          //   });
+          //   commit("BACKUPS_UPDATE", newList);
+          // } catch (error) {
+          //   commit("BACKUPS_ERROR", error);
+          // }
+          commit("BACKUPS_UPDATE", result.data.items);
+        } else {
+          commit("BACKUPS_ERROR", "Return Status: " + result.status);
+        }
+      })
+      .catch(error => {
+        commit("BACKUPS_ERROR", error);
+      });
+  },
+  deleteBackups({ commit }, backups) {
+    console.log(backups);
+    backups.forEach(backup => {
+      axios
+        .delete(`/api/apis/velero.io/v1/namespaces/velero/backups/${backup}`)
+        .then(result => {
+          console.log(result);
+        })
+        .catch(error => {
+          commit("BACKUPS_ERROR", error);
+        });
+    });
   }
 };
 
 const mutations = {
   BACKUPS_UPDATE(state, backups) {
     state.backups = backups;
+    state.keys = flattenObject(backups[0]);
     state.loading = false;
   },
   BACKUPS_LOADING(state) {
--- src/views/Backups.vue
+++ src/views/Backups.vue
@@ -11,6 +11,43 @@
         <v-btn icon @click="getBackups()">
           <v-icon>mdi-refresh</v-icon>
         </v-btn>
+        <v-btn icon @click="deleteBackups(selectedItemsName)">
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+        <v-menu :close-on-content-click="false" max-height="500px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list subheader dense flat>
+            <v-list-item-group
+              v-model="settings"
+              multiple
+              v-on:change="updateRow()"
+            >
+              <template v-for="(value, index) in keysList">
+                <v-list-item :key="index" :value="value">
+                  <template v-slot:default="{ active, toggle }">
+                    <v-list-item-action>
+                      <v-checkbox
+                        :input-value="active"
+                        color="primary"
+                        @click="toggle"
+                      ></v-checkbox>
+                    </v-list-item-action>
+
+                    <v-list-item-content>
+                      <v-list-item-title v-text="value">{{
+                        index
+                      }}</v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+                </v-list-item>
+              </template>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
       </v-card-title>
       <v-data-table
         :headers="headers"
@@ -21,35 +58,61 @@
         show-expand
         multi-sort
         :loading="loading"
+        :itemsPerPage="30"
+        itemKey="metadata.name"
+        v-on:input="selectedItems(selected)"
+        v-model="selected"
       >
-        <template v-slot:expanded-item="{ headers, item }">
+        <template v-slot:expanded-item="{ item }">
           <td :colspan="headers.length">
-            {{ item }}
+            <kbd>{{ item }}</kbd>
           </td>
         </template>
       </v-data-table>
+      <!-- <v-bottom-sheet hideOverlay :value="true" persistent>
+        <v-sheet class="text-center" height="200px">
+          <div class="my-3">{{ selectedItemsName }}</div>
+        </v-sheet>
+      </v-bottom-sheet> -->
     </v-card>
   </div>
 </template>
 
+<style scoped>
+/* #list {
+  max-height: 500px;
+} */
+</style>
+
 <script>
 import { mapGetters } from "vuex";
 export default {
-  name: "Home",
+  name: "Backups",
   components: {},
   data() {
     return {
       search: "",
+      selectedItemsName: [],
+      settings: [
+        "metadata.name",
+        "spec.ttl",
+        "spec.volumeSnapshotLocations",
+        "status.errors",
+        "metadata.creationTimestamp"
+      ],
       headers: [
         {
           text: "Name",
           align: "start",
-          value: "name"
+          value: "metadata.name"
         },
-        { text: "Ttl", value: "ttl" },
-        { text: "VolumeSnapshotLocations", value: "volumeSnapshotLocations" },
-        { text: "Errors", value: "errors" },
-        { text: "creationTimestamp", value: "creationTimestamp" }
+        { text: "Ttl", value: "spec.ttl" },
+        {
+          text: "VolumeSnapshotLocations",
+          value: "spec.volumeSnapshotLocations"
+        },
+        { text: "Errors", value: "status.errors" },
+        { text: "creationTimestamp", value: "metadata.creationTimestamp" }
       ]
     };
   },
@@ -57,16 +120,39 @@ export default {
     ...mapGetters({
       error: "backups/error",
       loading: "backups/isLoading",
-      backups: "backups/backups"
+      backups: "backups/backups",
+      keysList: "backups/keys"
     })
   },
   methods: {
     getBackups: function() {
-      this.$store.dispatch("backups/get");
+      this.$store.dispatch("backups/getBackups");
+    },
+    getBackup: function(id) {
+      this.$store.dispatch("backups/getBackup", id);
+    },
+    deleteBackups: function(backups) {
+      this.$store.dispatch("backups/deleteBackups", backups);
+      this.getBackups();
+    },
+    updateRow: function() {
+      this.headers = [];
+      this.settings.sort().forEach((value, index) => {
+        this.headers[index] = {
+          text: value.split(/[./]/).pop(),
+          align: "start",
+          value: value
+        };
+      });
+    },
+    selectedItems: function(items) {
+      if (!items[0]) return;
+      this.selectedItemsName = items.map(a => a.metadata.name);
     }
   },
   mounted() {
     this.getBackups();
+    // this.getBackup("everything-20200601155346");
   }
 };
 </script>
